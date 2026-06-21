731902e4d3c9d92b25d4bfb8cf86a98a1216247c
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-06-22 02:10:01 +0300

    update global styles, paths, heading component and dependencies

--- components/heading.tsx
+++ components/heading.tsx
@@ -6,8 +6,8 @@ type HeadingProps = {
 const Heading = ({ title, description }: HeadingProps) => {
   return (
     <>
-    <div className="ml-4 flex flex-col gap-1 pt-4">
-        <h1 className="text-2xl font-bold tracking-tight">{title}</h1>
+    <div className="ml-2 flex flex-col gap-y-1 pt-2">
+        <h1 className="text-2lg font-bold tracking-tight">{title}</h1>
         {description && (
           <p className="text-sm text-muted-foreground">
             {description}
