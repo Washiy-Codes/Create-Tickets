75eae8a4098e58101c2507263f13880e08628702
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-03 23:23:29 +0300

    feat: add ticket sorting with dynamic sortKey and sortValue

--- components/sort-select.tsx
+++ components/sort-select.tsx
@@ -1,41 +1,46 @@
 "use client";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+
 type options = {
   label: string;
-  value: string;
+  sortKey: string;
+  sortValue: string;
 };
-type SortSelectProps = {
-  defaultValue: string;
+
+type sortObject = {
+  sortKey: string;
+  sortValue: string;
+};
+export type SortSelectProps = {
   options: options[];
+  value: sortObject;
+  onChange: (value: sortObject) => void;
 };
 
-const SortSelect = ({defaultValue, options}: SortSelectProps) => {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
-const handleSort = (value: string) => {
-  const params = new URLSearchParams(searchParams);
-    if (value === defaultValue || !value) {
-    params.delete("sort");
-  } else {
-    params.set("sort", value);
-  }
+export type SortSelectOption = {
+  label: string;
+  sortKey: string;
+  sortValue: string;
+};
 
-  replace(`${pathname}?${params.toString()}`, {
-    scroll: false,
-  });
+const SortSelect = ({options, value, onChange}: SortSelectProps) => {  
+const handleSort = (compositeKey: string) => {
+  const [sortKey, sortValue] = compositeKey.split('-');
+
+  onChange({ sortKey, sortValue });
+  
 };
 
 return(
-    <Select defaultValue={searchParams.get("sort")?.toString() || defaultValue} onValueChange={handleSort}>
+    <Select defaultValue={value.sortKey + '-' + value.sortValue} onValueChange={handleSort}>
   <SelectTrigger className="w-32"> 
     <SelectValue />
   </SelectTrigger>
   <SelectContent>
     {options.map((option) => (
-      <SelectItem key={option.value} value={option.value}>
+      <SelectItem key={option.sortKey + option.sortValue} value={option.sortKey + '-' + option.sortValue}>
         {option.label}
       </SelectItem>
     ))}
@@ -68,3 +73,22 @@ export { SortSelect };
   //     scroll: false,
   //   });
   // };
+
+
+
+
+  // const searchParams = useSearchParams();
+
+// const pathname = usePathname();
+//   const { replace } = useRouter();
+
+  // const params = new URLSearchParams(searchParams);
+  //   if (value === defaultValue || !value) {
+  //   params.delete("sort");
+  // } else {
+  //   params.set("sort", value);
+  // }
+
+  // replace(`${pathname}?${params.toString()}`, {
+  //   scroll: false,
+  // });
