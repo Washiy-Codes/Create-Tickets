19d2c8decb8b30dc1cf275ab425ff9f4fe4e1bf7
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-07 22:48:48 +0300

    feat(password): add password reset routes and schema changes

--- paths.tsx
+++ paths.tsx
@@ -5,7 +5,19 @@ const ticketEditPath = (ticketId: string) => `/tickets/${ticketId}/edit`;
 const signUpPath = () => "/sign-up";
 const signInPath = () => "/sign-in";
 const forgotPasswordPath = () => "/forgot-password";
+const resetPasswordPath = () => "/reset-password";
+
 const accountProfilePath = ()=> "/account/profile";
 const accountPasswordPath = ()=> "/account/password";
 
-export { ticketsPath, ticketPath, homePath, ticketEditPath, signUpPath, signInPath, forgotPasswordPath, accountProfilePath, accountPasswordPath };
+export { 
+    ticketsPath, 
+    ticketPath, 
+    homePath, ticketEditPath, 
+    signUpPath, 
+    signInPath, 
+    forgotPasswordPath, 
+    accountProfilePath, 
+    accountPasswordPath,
+    resetPasswordPath
+};
