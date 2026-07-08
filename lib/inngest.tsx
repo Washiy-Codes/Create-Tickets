a73a2f2b27ad4d0b36e92685b8467766c42c232f
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-08 17:05:44 +0300

    Setup Inngest client

---
+++ lib/inngest.tsx
@@ -0,0 +1,9 @@
+
+import { Inngest, eventType, staticSchema } from "inngest"
+import { passwordResetEvent } from "@/features/password/event/event-password-reset"
+
+export const inngest = new Inngest({ id: "create-tickets-app" })
+
+export const passwordResetPayload = eventType("app/password.reset-password", {
+    schema: staticSchema<passwordResetEvent>()
+})
